8baed339ee12c692e06a538959eb08d5761ee24e
jsy4 <65105285+jsy4@users.noreply.github.com>
2022-09-28 19:24:12 +0900

    main

--- Project_Cafe/src/MainTest.java
+++ Project_Cafe/src/MainTest.java
@@ -12,6 +12,7 @@ public class MainTest extends JFrame{
 	
 	MainTest(){
 		setTitle("PC방");
+		panel_main = new Panel_Main();
 		
 		
 	}
@@ -21,15 +22,13 @@ public class MainTest extends JFrame{
 
 		JTabbedPane tab = new JTabbedPane();
 		tab.addTab("main", panel_main);
-		
 		tab.addTab("라면", panel_noodle);
 		tab.addTab("분식", panel_food);
 		tab.addTab("간식", panel_snack);
 		tab.addTab("음료", panel_drink);
-	
 		add(tab,BorderLayout.CENTER);
 		
-		setBounds(100, 100, 700, 900);
+		setBounds(100, 100, 900, 700);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
--- Project_Cafe/src/Panel_Main.java
+++ Project_Cafe/src/Panel_Main.java
@@ -1,9 +1,29 @@
 import java.awt.Color;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Panel_Main extends JPanel{
+	JButton [] bMenu = new JButton[4];
+
 	public Panel_Main() {
-		setBackground(Color.CYAN);
+		setLayout(new GridLayout());
+		String a[] = { "라면", "분식", "간식", "음료" };
+		for(int i =0; i <bMenu.length; i++) {
+			bMenu[i] = new JButton(a[i]);
+		}//for
+		addLayout();
+		eventProc();
+	}//Panel_Main
+
+	void addLayout() {
+		for(int i =0; i <bMenu.length; i++) {
+			add(bMenu[i]);
+		}//for
+	}//addLayout
+	
+	void eventProc() {
+		
 	}
 }
