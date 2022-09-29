232f880502c0dc768806f085e64f704611bf805e
jsy4 <65105285+jsy4@users.noreply.github.com>
2022-09-29 17:21:53 +0900

    taJang and list became parent(Panel_Main)

--- Project_Cafe/src/EdibleVo.java
+++ Project_Cafe/src/EdibleVo.java
@@ -1,56 +1,34 @@
-<<<<<<< HEAD
-public class EdibleVo {
-	String name;
-	int cost;
-	public EdibleVo(String name, int cost) {
-		super();
-		this.name = name;
-		this.cost = cost;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getCost() {
-		return cost;
-	}
-	public void setCost(int cost) {
-		this.cost = cost;
-	}
-	@Override
-	public String toString() {
-		return "[name=" + name + ", cost=" + cost + "]\n";
-	}
-	
-}
-=======
 
 public class EdibleVo {
 	String name;
 	int cost;
+
 	public EdibleVo(String name, int cost) {
 		super();
 		this.name = name;
 		this.cost = cost;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getCost() {
 		return cost;
 	}
+
 	public void setCost(int cost) {
 		this.cost = cost;
 	}
+
 	@Override
 	public String toString() {
 		return "[name=" + name + ", cost=" + cost + "]\n";
 	}
-	
+ 
+
 }
->>>>>>> branch 'main' of https://github.com/jsy4/KosmoMidProject-Cafe.git
