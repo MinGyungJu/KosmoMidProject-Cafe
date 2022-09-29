457438535bda844f36c91bf6573a23f91320de48
jsy4 <65105285+jsy4@users.noreply.github.com>
2022-09-29 17:53:22 +0900

    Update MainTest.java

--- Project_Cafe/src/MainTest.java
+++ Project_Cafe/src/MainTest.java
@@ -1,10 +1,14 @@
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MainTest extends JFrame{
 	Panel_Main panel_main; 
@@ -15,7 +19,12 @@ public class MainTest extends JFrame{
 	JTabbedPane tab = new JTabbedPane();
 	public ArrayList<EdibleVo> list = new ArrayList<EdibleVo>();
 	public JTextArea taJang = new JTextArea();
+	public JTextField tf;// 직원호출
+	public JButton sosbt,addbt, addbt2; 
+	public JLabel banner1 = new JLabel("요청 쓴후에 호출, 장바구니에는 하나씩만");
+	public JTextArea ta; // 장바구니 담기취소
 
+	
 	MainTest(){
 		setTitle("PC방");
 		panel_main = new Panel_Main(this);
@@ -37,6 +46,29 @@ public class MainTest extends JFrame{
 		tab.addTab("음료", panel_drink);
 		add(tab,BorderLayout.CENTER);
 		
+		//
+//		JPanel pEast = new JPanel();
+//		JButton b = new JButton("확인");
+//		pEast.add(b);
+//		add(pEast, BorderLayout.EAST);
+		// ----------------------------오른쪽패널에 직원호출 및 장바구니
+		JPanel pEast = new JPanel();
+		pEast.setLayout(new GridLayout(2, 1));
+		pEast.setPreferredSize(new java.awt.Dimension(300, 450));
+
+		JPanel pEast1 = new JPanel();
+		pEast1.add(tf); // 요청 사항 입력 후, 직원 호출을 눌러주세요
+		pEast1.add(sosbt); // 직원 호출
+
+		JPanel pEast2 = new JPanel();
+		pEast2.add(banner1); // 이벤트 배너 삽입
+		pEast2.add(ta); // 주문내역 출력
+		pEast2.add(addbt); // 장바구니 담기
+		pEast2.add(addbt2); // 장바구니 취소
+		pEast2.add(taJang); // 주문내역 출력
+		pEast.add(pEast1);
+		pEast.add(pEast2);
+		add(pEast, BorderLayout.EAST);
 	
 		
 		setBounds(100, 100, 900, 700);
