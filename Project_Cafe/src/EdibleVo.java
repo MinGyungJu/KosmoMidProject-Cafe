2068f2a2bcd3aa48a9d98fb5cf387d6ceba0cd7d
nononoh87 <112688283+nononoh87@users.noreply.github.com>
2022-09-29 16:34:33 +0900

    2022

---
+++ Project_Cafe/src/EdibleVo.java
@@ -0,0 +1,26 @@
+public class EdibleVo {
+	String name;
+	int cost;
+	public EdibleVo(String name, int cost) {
+		super();
+		this.name = name;
+		this.cost = cost;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	@Override
+	public String toString() {
+		return "[name=" + name + ", cost=" + cost + "]\n";
+	}
+	
+}
--- Project_Cafe/src/Panel_Noodle.java
+++ Project_Cafe/src/Panel_Noodle.java
@@ -1,48 +1,70 @@
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Panel_Noodle extends JPanel {
-	
+
 	MainTest parent;
 	JFrame f;
 	JTextField tf;
-	JTextArea ta;
-	JButton bnd1, bnd2, bnd3, bnd4, bnd5, bnd6, badd, bcan, bpay;
-	
+	JTextArea ta,taJang;
+	JButton bnd1, bnd2, bnd3, bnd4, bnd5, bnd6, bcan, bpay, addbt, sosbt;
+	ArrayList<EdibleVo> list = new ArrayList<EdibleVo>();
+
+	JLabel banner1 = new JLabel("요청사항을 쓴 후에 호출버튼을 누르세요. 장바구니에는 하나씩만 담으세요");
+	JLabel banner2 = new JLabel();
+	JLabel banner3 = new JLabel();
+	JLabel banner4 = new JLabel();
+	JLabel banner5 = new JLabel();
+
 	public Panel_Noodle(MainTest mainTest) {
 		parent = mainTest;
-		setBackground(Color.yellow);
+//		setBackground(Color.yellow);
 
 		f = new JFrame();
 		tf = new JTextField();
 		ta = new JTextArea();
-		//메뉴 관련버튼 (이미지 경로)
-		bnd1 = new JButton(new ImageIcon("src\\imgs\\1.PNG"));
-		bnd2 = new JButton(new ImageIcon("src\\imgs\\2.PNG"));
-		bnd3 = new JButton(new ImageIcon("src\\imgs\\3.PNG"));
-		bnd4 = new JButton(new ImageIcon("src\\imgs\\4.PNG"));
-		bnd5 = new JButton(new ImageIcon("src\\imgs\\5.PNG"));
-		bnd6 = new JButton(new ImageIcon("src\\imgs\\6.PNG"));
-		//주문 관련 버튼
-		badd = new JButton("장바구니 담기");	
-		bcan = new JButton("취소");	
-		bpay = new JButton("결제");	
-
+		taJang = new JTextArea();
+		tf.setPreferredSize(new Dimension(250,200));
+		taJang.setPreferredSize(new Dimension(250,200));
+		// 메뉴 관련버튼 (이미지 경로)
+		bnd1 = new JButton("너구리:3500원", new ImageIcon("src\\imgs_noodle\\1.PNG"));
+		bnd2 = new JButton("신라면:3500원", new ImageIcon("src\\imgs_noodle\\2.PNG"));
+		bnd3 = new JButton("오징어짬뽕:3500원", new ImageIcon("src\\imgs_noodle\\3.PNG"));
+		bnd4 = new JButton("튀김우동:3500원", new ImageIcon("src\\imgs_noodle\\4.PNG"));
+		bnd5 = new JButton("진라면:3000", new ImageIcon("src\\imgs_noodle\\5.PNG"));
+		bnd6 = new JButton("짜파게티:3500원", new ImageIcon("src\\imgs_noodle\\6.PNG"));
+		// 주문 관련 버튼
+		bcan = new JButton("모두 취소");
+		bpay = new JButton("결제");
+		addbt = new JButton("장바구니 담기");
+		sosbt = new JButton("직원호출");
 		addLayout();
 		eventProc();
 
-	} //Panel_Noodle()
+	} // Panel_Noodle()
 
 	void addLayout() {
+//		BorderLayout layout = new BorderLayout();
+//		setLayout(layout); 
+//		remove(layout.getLayoutComponent(BorderLayout.CENTER));
 		setLayout(new BorderLayout());
+
 		JPanel pWest = new JPanel();
-		pWest.setLayout(new GridLayout(3, 2, 5, 5));
-		pWest.setPreferredSize(new java.awt.Dimension(500, 500));
+		pWest.setLayout(new GridLayout(3, 2, 10, 10));
+		pWest.setPreferredSize(new java.awt.Dimension(500, 600));
 		pWest.add(bnd1);
 		pWest.add(bnd2);
 		pWest.add(bnd3);
@@ -51,66 +73,126 @@ public class Panel_Noodle extends JPanel {
 		pWest.add(bnd6);
 
 		add(pWest, BorderLayout.WEST);
-		
+
 		JPanel pNorth = new JPanel();
-		pNorth.setLayout(new GridLayout(1,2));
+		pNorth.setLayout(new GridLayout(1, 2));
 		pNorth.add(tf);
 		add(pNorth, BorderLayout.NORTH);
-		
+		// ----------------------------------------------
 		JPanel pEast = new JPanel();
-		pEast.setLayout(new GridLayout(2,1));
-		pEast.setPreferredSize(new java.awt.Dimension(380,0));
-		pEast.add(ta);
-		pEast.add(badd);
+
+		pEast.setLayout(new GridLayout(2, 1));
+		pEast.setPreferredSize(new java.awt.Dimension(300, 450));
+
+		JPanel pEast1 = new JPanel();
+
+		pEast1.add(tf); // 요청 사항 입력 후, 직원 호출을 눌러주세요
+		pEast1.add(sosbt); // 직원 호출
+		JPanel pEast2 = new JPanel();
+		pEast2.add(banner1); // 이벤트 배너 삽입
+		pEast2.add(ta); // 주문내역 출력
+		pEast2.add(addbt); // 장바구니 담기
+		pEast2.add(taJang); // 주문내역 출력
+		pEast.add(pEast1);
+		pEast.add(pEast2);
+
 		add(pEast, BorderLayout.EAST);
-		
-		
+		// ----------------------------------------------
 		JPanel pSouth = new JPanel();
-		
-		pSouth.setLayout(new GridLayout(1,2));
+
+		pSouth.setLayout(new GridLayout(1, 2));
 		pSouth.setPreferredSize(new java.awt.Dimension(10, 50));
 		pSouth.add(bcan);
 		pSouth.add(bpay);
 		add(pSouth, BorderLayout.SOUTH);
 
-	} //addLayout()
+	} // addLayout()
 
 	void eventProc() {
 		bnd1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "너구리라면 : 3500원");
-			}//actionPerformed
-		});//addActionListener
+				ta.append("너구리 3500");
+			}// actionPerformed
+		});// addActionListener
 		bnd2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "신라면 : 3500원");
-			}//actionPerformed
-		});//addActionListener
+				ta.append("신라면 3500");
+			}// actionPerformed
+		});// addActionListener
 		bnd3.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "오징어짬뽕 : 3500원");
-			}//actionPerformed
-		});//addActionListener
+				ta.append("오징어짬뽕 3500");
+			}// actionPerformed
+		});// addActionListener
 		bnd4.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "튀김우동 : 3500원");
-			}//actionPerformed
-		});//addActionListener
+				ta.append("튀김우동 3000");
+			}// actionPerformed
+		});// addActionListener
 		bnd5.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "진라면 : 3500원");
-			}//actionPerformed
-		});//addActionListener
+				ta.append("진라면 3000");
+			}// actionPerformed
+		});// addActionListener
 		bnd6.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "짜파게티 : 3500원");
-			}//actionPerformed
-		});//addActionListener
-	}//eventProc
-}//Panel_Main
+				ta.append("짜파게티 3500");
+			}// actionPerformed
+		});// addActionListener
+		addbt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				inputData();
+			}// actionPerformed
+		});// addActionListenerAddbt
+		bpay.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pay();
+			}// actionPerformed
+		});// addActionListenerbpay
+		bcan.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cancelAll();
+			}// actionPerformed
+		});// addActionListenerbcan
+		sosbt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String s= tf.getText();
+				JOptionPane.showMessageDialog(null,"용무 "+ s +"로 종업원 호출하였습니다. 기다리세요.");
+				tf.setText("");
+			}// actionPerformed
+		});// addActionListenersosbt
+	}// eventProc
+	void inputData() {
+		String a = ta.getText();
+		String[] b = a.split(" ");
+		EdibleVo v = new EdibleVo(b[0],Integer.valueOf(b[1]));
+		list.add(v);
+		ta.setText("");
+		taJang.append(v.toString());
+	}//input dataa
+	
+	void pay() {
+		int total = 0;
+		for(EdibleVo v : list) {
+			total += v.getCost();
+		}//for
+		JOptionPane.showMessageDialog(null, "내실 총 금액은 "+ total+" 원입니다.");
+		cancelAll();
+	}//pay
+	
+	void cancelAll() {
+		taJang.setText("");
+		ta.setText("");
+		list = new ArrayList<EdibleVo>();
+	}
+}// panel_drink
