8ca4618d30d47a013e15c95414319deedd9b5d28
jsy4 <65105285+jsy4@users.noreply.github.com>
2022-09-30 09:35:41 +0900

    main simplification

--- Project_Cafe/src/Panel_Main.java
+++ Project_Cafe/src/Panel_Main.java
@@ -9,60 +9,44 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Panel_Main extends JPanel{
-	JButton [] bMenu = new JButton[4];
+public class Panel_Main extends JPanel {
+	JButton[] bMenu = new JButton[4];
 	MainTest parent;
 	public ArrayList<EdibleVo> list = new ArrayList<EdibleVo>();
 
 	public Panel_Main(MainTest mainTest) {
 		parent = mainTest;
-		setLayout(new GridLayout(2,2));
-		//making buttons
+		setLayout(new GridLayout(2, 2));
+		// making buttons
 		String a[] = { "라면", "분식", "간식", "음료" };
-		for(int i =0; i <bMenu.length; i++) {
+		for (int i = 0; i < bMenu.length; i++) {
 			bMenu[i] = new JButton(a[i]);
-		}//for
-		String []b = {"src\\imgs\\m1.PNG","src\\imgs\\m2.PNG","src\\imgs\\m3.PNG",
-				"src\\imgs\\m4.PNG"};
-		for(int i =0; i < b.length; i++) {
+		} // for
+		String[] b = { "src\\imgs\\m1.PNG", "src\\imgs\\m2.PNG", "src\\imgs\\m3.PNG", "src\\imgs\\m4.PNG" };
+		for (int i = 0; i < b.length; i++) {
 			bMenu[i] = new JButton(new ImageIcon(b[i]));
 			bMenu[i].setBackground(new Color(255, 255, 255));
 		}
-		
+
 		addLayout();
 		eventProc();
-	}//Panel_Main
+	}// Panel_Main
 
 	void addLayout() {
-		for(int i =0; i <bMenu.length; i++) {
+		for (int i = 0; i < bMenu.length; i++) {
 			add(bMenu[i]);
-		}//for
-	}//addLayout
-	
+		} // for
+	}// addLayout
+
 	void eventProc() {
-		bMenu[0].addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				parent.tab.setSelectedIndex(1); 
-			}//actionPerformed
-		});//addActionListener
-		bMenu[1].addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				parent.tab.setSelectedIndex(2);
-			}//actionPerformed
-		});//addActionListener
-		bMenu[2].addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				parent.tab.setSelectedIndex(3);
-			}//actionPerformed
-		});//addActionListener
-		bMenu[3].addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				parent.tab.setSelectedIndex(4);
-			}//actionPerformed
-		});//addActionListener
-	}//eventProc
-}//Panel_Main
+		for (int i = 0; i < bMenu.length; i++) {
+			int j = i;
+			bMenu[i].addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					parent.tab.setSelectedIndex(j + 1);
+				}// actionPerformed
+			});// addActionListener
+		} // for
+	}// eventProc
+}// Panel_Main
